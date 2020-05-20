d97badf0b90d78eb9e67d50dc9e314ea8d68cf05
Elian <elianmarianogabriel@gmail.com>
2020-05-20 10:28:32 -0300

    binary tree methods created

--- binary_tree.c
+++ binary_tree.c
@@ -1,4 +1,28 @@
-/*
-    This file will handle the binary tree operations
-    such as searchs, and storing the binary tree itself.
-*/
+#include "binary_tree.h"
+#include <string.h>
+
+void set_empty_left_child(struct Node p, char token[20]){
+    struct Node *l;
+    strcpy(l->token, token);
+    l->left = NULL;
+    l->right = NULL;
+
+    p.left = l;
+}
+
+void set_empty_right_child(struct Node p, char token[20]){
+    struct Node *r;
+    strcpy(r->token, token);
+    r->left = NULL;
+    r->right = NULL;
+
+    p.right = r;
+}
+
+struct Node* get_left_child(struct Node p){
+    return p.left;
+}
+
+struct Node* get_right_child(struct Node p){
+    return p.right;
+}
--- binary_tree.h
+++ binary_tree.h
@@ -1,1 +1,22 @@
-// This is the header file for binary_tree.c
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H
+
+struct Node{
+    char token[20];
+    struct Node *left;
+    struct Node *right;
+};
+
+// Sets an empty left child
+void set_empty_left_child(struct Node p, char token[20]);
+
+// Sets an empty right child
+void set_empty_right_child(struct Node p, char token[20]);
+
+// Gets the left child
+struct Node* get_left_child(struct Node p);
+
+// Gets the right child
+struct Node* get_right_child(struct Node p);
+
+#endif
